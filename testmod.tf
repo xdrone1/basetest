95b7dd41b02fc1e98e3a45e5f36b7ddd4c19b609
xdrone1 <86126726+xdrone1@users.noreply.github.com>
2022-08-17 00:46:40 +0530

    my commit message

--- testmod.tf
+++ testmod.tf
@@ -1,35 +1,35 @@
 variable "rg1_name" {
   #Resource Group Name  
   description = "Please provide RG1 Name"
-  default = ""  
+  default = "rg_89"  
 }
 
 variable "rg1_location" {
 #Resource Group Location 
-default = ""
+default = "eastus"
 }
 
 variable "east_vNet01_name" {
 #vNet Name  
-default = ""
+default = "vmet_84"
 }
 
 variable "vnet01_internal_subnet01" {
 #Subnet Name  
-default = ""
+default = "737.848.38"
 }
 
 variable "Test-VM01-Name" {
 #VM Name 
-default = ""
+default = "vm_655"
 } 
 
 variable "vm_size_name" {
 #VM Size 
-default = ""
+default = "standard_b2s"
  }
 variable "east_nsg_name" {
-default = ""   
+default = "nsg_573"   
  } 
 
 variable "vnet01_address" {
